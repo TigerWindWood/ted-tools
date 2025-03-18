58ccbbfd239c8b3146a8fc8793c2fd1c1d97c0f9
Ian Sillitoe <ian@sillit.com>
2025-03-18 15:43:58 +0000

    allow 7 columns in new chainsaw output

--- ted_consensus_1.0/scripts/filter_domains.py
+++ ted_consensus_1.0/scripts/filter_domains.py
@@ -21,7 +21,13 @@ def main():
         for i, line in enumerate(f):
             line = line.rstrip('\n')
 
-            target, md5, nres, ndom, chopping, score = line.split()
+            cols = line.split()
+            if len(cols) == 6:
+                target, md5, nres, ndom, chopping, score = line.split()
+            elif len(cols) == 7:
+                # chainsaw has added a "time taken" column
+                target, md5, nres, ndom, chopping, score, _time = line.split()
+            
             if target == 'chain_id':
                 continue
 
